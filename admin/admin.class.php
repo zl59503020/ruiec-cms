45b6dfd7c591673dedfb7e6c324a8ff87aba112f
zl59503020 <zl59503020@gmail.com>
2012-12-29 10:20:40 +0000

    2012-12-29 18:20

--- admin/admin.class.php
+++ admin/admin.class.php
@@ -6,6 +6,8 @@ class admin {
 	var $founderid;
 	var $db;
 	var $pre;
+	var $table;
+	var $fields;
 	var $errmsg = '';
 
 	function admin() {
@@ -13,12 +15,14 @@ class admin {
 		$this->founderid = $CFG['founderid'];
 		$this->db = &$db;
 		$this->pre = $db->pre;
+		$this->table = $db->pre.'member';
+		$this->fields = array('username','password','email','groupid','status','truename','sex','company','phone','qq','logincount','lastlogintime','lastloginip','time','ip','userAgent','other');
 	}
 
 	// 获取
 	function get_one($user, $type = 0) {
 		$fields = $type ? 'username' : 'userid';
-        return $this->db->get_one("SELECT * FROM {$this->pre}member WHERE `$fields`='$user'");
+        return $this->db->get_one("SELECT * FROM {$this->table} WHERE `$fields`='$user'");
 	}
 	
 	// 删除
@@ -26,7 +30,7 @@ class admin {
 		$r = $this->get_one($userid);
 		if($r){
 			if($r['userid'] == $this->founderid) die('创始人不可删除!');
-			$this->db->query("DELETE FROM {$this->pre}member WHERE userid=$userid");
+			$this->db->query("DELETE FROM {$this->table} WHERE userid=$userid");
 			return true;
 		} else {
 			die('管理员不存在');
@@ -37,24 +41,65 @@ class admin {
 	function get_list($condition='') {
 		$admins = array();
 		if($condition != '') $condition = ' WHERE '.$condition;
-		$result = $this->db->query("SELECT * FROM {$this->pre}member $condition ORDER BY userid ASC");
+		$result = $this->db->query("SELECT * FROM {$this->table} $condition ORDER BY userid ASC");
 		while($r = $this->db->fetch_array($result)) {
-			$r['lastlogintime'] = timetodate($r['lastlogintime'], 5);
+			$r['lastlogintime'] = $r['lastlogintime'] == '' ? '-' : timetodate($r['lastlogintime'], 5);
 			$gpinfo = $this->db->get_one("SELECT name FROM {$this->pre}groups WHERE itemid = {$r['groupid']}");
 			$r['groupname'] = $gpinfo == null ? '未分组' : $gpinfo['name'];
+			$r['truename'] = $r['truename'] == '' ? '匿名' : $r['truename'];
 			$admins[] = $r;
 		}
 		return $admins;
 	}
+	
+	// 检测.必填
+	function pass($post) {
+		if(!is_array($post)) return false;
+		if(!$post['groupid']) return $this->_('选择分组!');
+		if(strlen($post['username']) < 3) return $this->_('用户名非法!');
+		return true;
+	}
+	
+	// 检测.默认
+	function set($post,$ia=true) {
+		global $RE_TIME, $RE_IP;
+		if($ia){
+			$post['ip'] = $RE_IP;
+			$post['time'] = $RE_TIME;
+			$post['userAgent'] = $_SERVER['HTTP_USER_AGENT'];
+		}else{
+			if(isset($post['groupid'])) unset($post['groupid']);
+			if(isset($post['username'])) unset($post['username']);
+			if($post['password'] == '') unset($post['password']);
+		}
+		if(isset($post['password'])) $post['password'] = md5($post['password']);
+		if(isset($post['other'])) $post['other'] = serialize($post['other']);
+		return $post;
+	}
 
 	// 添加
-	function add($userid, $right, $admin) {
-		
+	function add($post) {
+		$post = $this->set($post);
+		$sqlk = $sqlv = '';
+		foreach($post as $k=>$v) {
+			if(in_array($k, $this->fields)) { $sqlk .= ','.$k; $sqlv .= ",'$v'"; }
+		}
+        $sqlk = substr($sqlk, 1);
+        $sqlv = substr($sqlv, 1);
+		$this->db->query("INSERT INTO {$this->table} ($sqlk) VALUES ($sqlv)");
+		return $this->db->insert_id();
 	}
 
 	// 编辑
-	function edit($right, $type = 0) {
-		
+	function edit($post,$userid) {
+		$post = $this->set($post,false);
+		$sql = '';
+		foreach($post as $k=>$v) {
+			if(in_array($k, $this->fields)) $sql .= ",$k='$v'";
+		}
+        $sql = substr($sql, 1);
+	    $this->db->query("UPDATE {$this->table} SET $sql WHERE userid=$userid");
+		return true;
 	}
 
 	// error
--- admin/template/admin_edit.tpl.php
+++ admin/template/admin_edit.tpl.php
@@ -4,7 +4,7 @@ include tpl('header');
 
 ?>
 
-	<div class="navigation">首页 &gt; 控制面板 &gt; <a href="javascript:;" onclick="_url('?file=<?php echo $file; ?>',{n:'sys_manager',t:'管理员管理'});">管理员管理</a> &gt; 添加管理员 </div>
+	<div class="navigation">首页 &gt; 控制面板 &gt; <a href="javascript:;" onclick="_url('?file=<?php echo $file; ?>',{n:'sys_manager',t:'管理员管理'});">管理员管理</a> &gt; 编辑管理员 </div>
 	<div id="contentTab">
 
 		<form action="" method="post" id="myform" name="myform" >
@@ -16,74 +16,70 @@ include tpl('header');
 						<tr>
 							<th>管理员分组</th>
 							<td>
-								<select name="post[groupid]" class="txtInput normal required">
-									<option value="">请选择分组...</option>
-						<?php	foreach($groups as $v){ ?>
-									<option value="<?php echo $v['itemid']; ?>"><?php echo $v['name']; ?></option>
-						<?php 	} ?>
-								</select>
+								<?php echo $groupname; ?>
 							</td>
 						</tr>
 						<tr>
 							<th>用户名</th>
 							<td>
-								<input type="text" name="post[username]" class="txtInput normal required" />
+								<?php echo $username; ?>
 							</td>
 						</tr>
 						<tr>
-							<th>密码</th>
+							<th>新密码</th>
 							<td>
-								<input type="password" id="v_pwd" name="post[password]" class="txtInput normal required" />
+								<input type="password" name="post[password]" value="" class="txtInput normal" />
+								<?php echo tips('如果不修改密码请勿修改!'); ?>
 							</td>
 						</tr>
 						<tr>
-							<th>确认密码</th>
+							<th>原密码</th>
 							<td>
-								<input type="password" class="txtInput normal required" />
+								<input type="password" name="post[passwords]" value="" class="txtInput normal" />
 							</td>
 						</tr>
 						<tr>
 							<th>邮箱</th>
 							<td>
-								<input type="text" name="post[email]" class="txtInput normal email required" />
+								<input type="text" name="post[email]" value="<?php echo $email; ?>" class="txtInput normal email required" />
 							</td>
 						</tr>
 						<tr>
 							<th>冻结</th>
 							<td>
-								<input type="radio" name="post[status]" value="0" /> 是&nbsp;&nbsp;&nbsp;&nbsp; 
-								<input type="radio" name="post[status]" value="1" checked /> 否
+								<input type="radio" name="post[status]" value="0" <?php echo $status == '0' ? 'checked' : ''; ?> /> 是&nbsp;&nbsp;&nbsp;&nbsp; 
+								<input type="radio" name="post[status]" value="1" <?php echo $status == '1' ? 'checked' : ''; ?> /> 否
 							</td>
 						</tr>
 						<tr>
 							<th>真实姓名</th>
 							<td>
-								<input type="text" name="post[truename]" class="txtInput normal" />
+								<input type="text" name="post[truename]" value="<?php echo $truename; ?>" class="txtInput normal" />
 							</td>
 						</tr>
 						<tr>
 							<th>性别</th>
 							<td>
-								<input type="radio" name="post[sex]" value="1" checked /> 男&nbsp;&nbsp;&nbsp;&nbsp; 
-								<input type="radio" name="post[sex]" value="0" /> 女
+								<input type="radio" name="post[sex]" value="1" <?php echo $sex=='1'?'checked':''; ?> /> 男&nbsp;&nbsp;&nbsp;&nbsp; 
+								<input type="radio" name="post[sex]" value="0" <?php echo $sex == '0' ? 'checked' : ''; ?> /> 女
 							</td>
 						</tr>
 						<tr>
 							<th>公司</th>
 							<td>
-								<input type="text" name="post[company]" class="txtInput normal" />
+								<input type="text" name="post[company]" value="<?php echo $company; ?>" class="txtInput normal" />
 							</td>
 						</tr>
 						<tr>
 							<th>电话</th>
 							<td>
-								<input type="text" name="post[phone]" class="txtInput normal" />
+								<input type="text" name="post[phone]" value="<?php echo $phone; ?>" class="txtInput normal" />
 							</td>
 						</tr>
 						<tr>
 							<th>QQ号码</th>
 							<td>
-								<input type="text" name="post[qq]" class="txtInput normal" />
+								<input type="text" name="post[qq]" value="<?php echo $qq; ?>" class="txtInput normal" />
 							</td>
 						</tr>
 					</tbody>
@@ -104,7 +100,7 @@ include tpl('header');
 <script type="text/javascript">
    //表单验证
     $(function () {
-       form_check_init('','',{title:'添加'});
+       form_check_init('','',{title:'修改'});
     });
 
 </script>
